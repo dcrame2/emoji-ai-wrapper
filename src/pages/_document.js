f35b9e71c9b5fa8b71a65f6679e823d8b6763c36
Dylan Cramer <dcrame2@gmail.com>
2024-09-16 20:58:44 -0500

    add google ads script

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,6 +4,11 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3875566048492186"
+        crossorigin="anonymous"
+      ></script>
       <body className="antialiased">
         <Main />
         <NextScript />
